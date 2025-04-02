0a4dbd172e1ab76769c40d0101f876982346fbe2
Cristian Acosta <cristian97acosta@gmail.com>
2025-04-02 15:37:46 -0600

    Finished API

--- API/main.py
+++ API/main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from typing import List
 from database import SessionLocal
 import models
+import datetime
 
 app = FastAPI()
 db = SessionLocal()
@@ -38,6 +39,36 @@ class Proveedor(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class Venta(BaseModel):
+    venta_id: int
+    fecha: datetime.datetime
+    producto_id: int
+    cantidad: int
+    precio_unitario: float
+    cliente_id: int
+    sucursal_id: int
+    total: float
+
+    class Config:
+        orm_mode = True
+        json_encoders = {
+            datetime: lambda v: v.isoformat()  # Converts datetime to ISO format when converting to JSON
+        }
+
+class Logistica(BaseModel):
+    envio_id: int
+    venta_id: int
+    fecha_envio: datetime.datetime
+    proveedor_id: int
+    estado_envio: str
+
+    class Config:
+        orm_mode = True
+        json_encoders = {
+            datetime: lambda v: v.isoformat()  # Converts datetime to ISO format when converting to JSON
+        }
 
 
 ######################## METHODS CLIENTES
@@ -78,6 +109,10 @@ def create_cliente(cliente:Cliente):
 @app.put('/clientes/{cliente_id}', response_model=Cliente, status_code=status.HTTP_200_OK)
 def update_cliente(cliente_id:int, cliente:Cliente):
     cliente_to_update = db.query(models.Client).filter(models.Client.cliente_id == cliente_id).first()
+
+    if cliente_to_update is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cliente does not exist")
+
     cliente_to_update.nombre = cliente.nombre
     cliente_to_update.edad = cliente.edad
     cliente_to_update.genero = cliente.genero
@@ -136,6 +171,10 @@ def create_producto(producto:Producto):
 @app.put('/productos/{producto_id}', response_model=Producto, status_code=status.HTTP_200_OK)
 def update_producto(producto_id:int, producto:Producto):
     producto_to_update = db.query(models.Product).filter(models.Product.producto_id == producto_id).first()
+
+    if producto_to_update is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Producto does not exist")
+
     producto_to_update.nombre_producto = producto.nombre_producto
     producto_to_update.categoria = producto.categoria
     producto_to_update.precio_base = producto.precio_base
@@ -193,6 +232,10 @@ def create_proveedor(proveedor:Proveedor):
 @app.put('/proveedores/{proveedor_id}', response_model=Proveedor, status_code=status.HTTP_200_OK)
 def update_proveedor(proveedor_id:int, proveedor:Proveedor):
     proveedor_to_update = db.query(models.Provider).filter(models.Provider.proveedor_id == proveedor_id).first()
+
+    if proveedor_to_update is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Proveedor does not exist")
+
     proveedor_to_update.nombre_proveedor = proveedor.nombre_proveedor
     proveedor_to_update.contacto = proveedor.contacto
     proveedor_to_update.ubicacion = proveedor.ubicacion
@@ -212,3 +255,135 @@ def delete_proveedor(proveedor_id:int):
     db.commit()
 
     return proveedor_to_delete
+
+######################## METHODS VENTAS
+@app.get('/ventas', response_model=List[Venta], status_code=200)
+def get_all_ventas():
+    ventas = db.query(models.Sale).all()
+    return ventas
+
+
+@app.get('/ventas/{venta_id}', response_model=Venta, status_code=status.HTTP_200_OK)
+def get_a_venta(venta_id:int):
+    venta = db.query(models.Sale).filter(models.Sale.venta_id == venta_id).first()
+    if venta is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Venta does not exist")
+    return venta
+
+
+@app.post('/ventas', response_model=Venta, status_code=status.HTTP_201_CREATED)
+def create_venta(venta:Venta):
+    db_venta = db.query(models.Sale).filter(models.Sale.venta_id == venta.venta_id).first()
+
+    if db_venta is not None:
+        raise HTTPException(status_code=400, detail="Venta already exists")
+
+    new_venta = models.Sale(
+        venta_id = venta.venta_id,
+        fecha = venta.fecha,
+        producto_id = venta.producto_id,
+        cantidad = venta.cantidad,
+        precio_unitario = venta.precio_unitario,
+        cliente_id = venta.cliente_id,
+        sucursal_id = venta.sucursal_id,
+        total = venta.total
+    )
+
+    db.add(new_venta)
+    db.commit()
+    return new_venta
+
+
+@app.put('/ventas/{venta_id}', response_model=Venta, status_code=status.HTTP_200_OK)
+def update_venta(venta_id:int, venta:Venta):
+    venta_to_update = db.query(models.Sale).filter(models.Sale.venta_id == venta_id).first()
+
+    if venta_to_update is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Venta does not exist")
+
+    venta_to_update.fecha = venta.fecha
+    venta_to_update.producto_id = venta.producto_id
+    venta_to_update.cantidad = venta.cantidad
+    venta_to_update.precio_unitario = venta.precio_unitario
+    venta_to_update.cliente_id = venta.cliente_id
+    venta_to_update.sucursal_id = venta.sucursal_id
+    venta_to_update.total = venta.total
+
+    db.commit()
+    return venta_to_update
+
+
+@app.delete('/ventas/{venta_id')
+def delete_venta(venta_id:int):
+    venta_to_delete = db.query(models.Sale).filter(models.Sale.venta_id == venta_id).first()
+
+    if venta_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Venta does not exist")
+
+    db.delete(venta_to_delete)
+    db.commit()
+
+    return venta_to_delete
+
+######################## METHODS LOGISTICA
+@app.get('/logistica', response_model=List[Logistica], status_code=200)
+def get_all_logistica():
+    logistica = db.query(models.Logistic).all()
+    return logistica
+
+
+@app.get('/logistica/{envio_id}', response_model=Logistica, status_code=status.HTTP_200_OK)
+def get_a_logistica(envio_id:int):
+    logistica = db.query(models.Logistic).filter(models.Logistic.envio_id == envio_id).first()
+    if logistica is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Envio does not exist")
+    return logistica
+
+
+@app.post('/logistica', response_model=Logistica, status_code=status.HTTP_201_CREATED)
+def create_logistica(logistica:Logistica):
+    db_logistica = db.query(models.Logistic).filter(models.Logistic.envio_id == logistica.envio_id).first()
+
+    if db_logistica is not None:
+        raise HTTPException(status_code=400, detail="Envio already exists")
+
+    new_logistica = models.Logistic(
+        envio_id = logistica.envio_id,
+        venta_id = logistica.venta_id,
+        fecha_envio = logistica.fecha_envio,
+        proveedor_id = logistica.proveedor_id,
+        estado_envio = logistica.estado_envio
+    )
+
+    db.add(new_logistica)
+    db.commit()
+    return new_logistica
+
+
+@app.put('/logistica/{envio_id}', response_model=Logistica, status_code=status.HTTP_200_OK)
+def update_logistica(envio_id:int, logistica:Logistica):
+    logistica_to_update = db.query(models.Logistic).filter(models.Logistic.envio_id == envio_id).first()
+
+    if logistica_to_update is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Envio does not exist")
+
+    logistica_to_update.venta_id = logistica.venta_id
+    logistica_to_update.fecha_envio = logistica.fecha_envio
+    logistica_to_update.proveedor_id = logistica.proveedor_id
+    logistica_to_update.estado_envio = logistica.estado_envio
+
+    db.commit()
+    return logistica_to_update
+
+
+@app.delete('/logistica/{envio_id}')
+def delete_logistica(envio_id:int):
+    logistica_to_delete = db.query(models.Logistic).filter(models.Logistic.envio_id == envio_id).first()
+
+    if logistica_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Envio does not exist")
+
+    db.delete(logistica_to_delete)
+    db.commit()
+
+    return logistica_to_delete
--- API/models.py
+++ API/models.py
@@ -1,5 +1,6 @@
 from database import Base
-from sqlalchemy import Column, Integer, Text, Numeric
+from sqlalchemy import Column, Integer, Text, Numeric, TIMESTAMP, ForeignKey
+
 
 class Client(Base):
     __tablename__ = 'clientes'
@@ -27,3 +28,26 @@ class Provider(Base):
     nombre_proveedor = Column(Text, index=True, nullable=False)
     contacto = Column(Text, index=True, nullable=False)
     ubicacion = Column(Text, nullable=False)
+
+
+class Sale(Base):
+    __tablename__ = 'ventas'
+
+    venta_id = Column(Integer, primary_key=True, index=True, nullable=False)
+    fecha = Column(TIMESTAMP, nullable=False)
+    producto_id = Column(Integer, ForeignKey('productos.producto_id'))
+    cantidad = Column(Integer, nullable=False)
+    precio_unitario = Column(Numeric, nullable=False)
+    cliente_id = Column(Integer, ForeignKey('clientes.cliente_id'))
+    sucursal_id = Column(Integer, index=True, nullable=False)
+    total = Column(Numeric, nullable=False)
+
+
+class Logistic(Base):
+    __tablename__ = 'logistica'
+
+    envio_id = Column(Integer, primary_key=True, index=True, nullable=False)
+    venta_id = Column(Integer, ForeignKey('ventas.venta_id'))
+    fecha_envio = Column(TIMESTAMP, nullable=False)
+    proveedor_id = Column(Integer, ForeignKey('proveedores.proveedor_id'))
+    estado_envio = Column(Text, nullable=False)
